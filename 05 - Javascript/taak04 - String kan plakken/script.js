b3d2e726388f3cba23efe921b84935487e640e95
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-10-28 17:22:58 +0100

    save changes

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,6 @@
+let mijnVoornaam = "Fawaz";
+let mijnAchternaam = 'Alayesh';
+let mijnleeftijd="23";
+
+let volledigeZin ="Mijn naam is"  +mijnVoornaam + mijnAchternaam+ "en mijn leeftijd is" +mijnleeftijd+ "jaar";
+console.log(volledigeZin);

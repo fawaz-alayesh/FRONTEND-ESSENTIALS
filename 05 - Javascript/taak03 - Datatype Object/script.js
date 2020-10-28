32c3bbfc7591b871b7fa6bf9c066476a37222ce7
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-10-28 16:58:52 +0100

    save changes

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,10 @@
+console.log("student");
+let student= {
+    voornaam="Fawaz",
+    achternaam="Alayesh",
+    leeftijd="23",
+    geslacht="man",
+   
+}
+console.log (`mijn naam is ${student.voornaam}`);
+console.log(`Ik ben ${student.leeftijd} jaar oud.`);

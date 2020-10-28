f4c8b65f3d28f8b70bef8fe638c867f0a3a36050
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-10-28 19:00:40 +0100

    save changes

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -7,3 +7,8 @@ console.log(window.document);
 
 
 // document.querySelector("#poster1");
+var elem = document.querySelector('#poster2');
+
+// Set a background color
+elem.style.backgroundColor = 'yellow';
+

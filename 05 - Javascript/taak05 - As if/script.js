9652bd6b520feea0400f6f342cf6fe08ebeb51bc
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-10-28 17:41:25 +0100

    save changes

---
+++ 05 - Javascript/taak05 - As if/script.js
@@ -0,0 +1,9 @@
+let mijnleeftijd="23"
+let stemGerechtigeleeftijd = "18";
+
+if(mijnleeftijd > stemGerechtigeleeftijd){
+    console.log("Jij bent mag stemmen met de verkiezingen");
+}
+if(mijnleeftijd == stemGerechtigeleeftijd){
+    console.log("Jij bent mag stemmen met de verkiezingen");
+}
